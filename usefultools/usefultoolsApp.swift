12824225cbf2f1c6b86c69b2f49ddf6994020308
Luis Amorim <luis@amorim.bio.br>
2025-02-24 09:08:10 -0300

    Initial Commit

---
+++ usefultools/usefultoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  usefultoolsApp.swift
+//  usefultools
+//
+//  Created by Luis Amorim on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct usefultoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
